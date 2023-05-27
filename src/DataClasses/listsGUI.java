5bd0e423ab465c1dc54467bad6ac18ae487fa2bb
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-05-26 19:39:10 -0500

    Cambios en la vista de consulta de formularios

--- src/DataClasses/listsGUI.java
+++ src/DataClasses/listsGUI.java
@@ -1,9 +1,15 @@
 package DataClasses;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class listsGUI extends JFrame{
     private JPanel miPanel;
+    private JButton cerrarSesiónButton;
+    private JTable table1;
+    private JButton verDenunciaButton;
+
     public listsGUI(){
         setContentPane(miPanel);
         setTitle("UN campus sin acoso");
@@ -11,5 +17,12 @@ public class listsGUI extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
+        cerrarSesiónButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                login login = new login();
+            }
+        });
     }
 }
